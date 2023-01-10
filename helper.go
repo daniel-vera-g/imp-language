79e5bfa6f9658cb931a73bf550244564ed30ee2b
danielVg <danielveragi@gmail.com>
2023-01-10 13:19:48 +0100

    fix: errors in print helpers

--- helper.go
+++ helper.go
@@ -8,9 +8,9 @@ func printValueStatemnt(s ValState) {
 	fmt.Printf("\n S: [\t") // Begin the environment S
 	for k, v := range s {
 		if v.flag == ValueInt {
-			fmt.Printf(k, " : ", v.valI, "\t")
+			fmt.Print(k, " : ", v.valI, "\t")
 		} else if v.flag == ValueBool {
-			fmt.Printf(k, " : ", v.valB, "\t")
+			fmt.Print(k, " : ", v.valB, "\t")
 		} else {
 			fmt.Printf("Undefined value in S: %s", k)
 		}
@@ -23,10 +23,9 @@ func printTypeStatemnt(t TyState) bool {
 	fmt.Printf("\n G: [\t") // Begin the environment T
 	for k, v := range t {
 		if v == TyInt {
-			fmt.Printf(k, " : ", "int", "\t")
-		}
-		if v == TyBool {
-			fmt.Printf(k, " : ", "bool", "\t")
+			fmt.Print(k, " : ", "int", "\t")
+		} else if v == TyBool {
+			fmt.Print(k, " : ", "bool", "\t")
 		} else {
 			fmt.Printf("Undefined type in G: %s", k)
 			return false
